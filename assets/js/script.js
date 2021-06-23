61f1adcdd0e243b774286a476cb9644ad8142840
Nazala <nazala5151@gmail.com>
2021-06-23 10:59:17 -0500

    local storage with array instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,39 @@ $("#currentDay").text (today.format("dddd, MMMM Do"));
 // set time block values in an array
 var time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
- 
+// call to pull up local storage items
+var scheduleArray = JSON.parse(localStorage.getItem("scheduleStorage"));
+console.log(scheduleArray);
+  if (scheduleArray) {
+    scheduleArray.forEach(function(input) {
+      $(".time-block").each (function(index) {
+        if (input.time === index.id) {
+          index.innerHTML = input.events 
+        }
+      })
+    });
+}
+  else {
+    console.log("working?")
+    // define all time blocks individually
+    var scheduleArray = [ 
+      { time: "9", events: "", },
+      { time: "10", events: "", },
+      { time: "11", events: "", },
+      { time: "12", events: "", },
+      { time: "1", events: "", },
+      { time: "2", events: "", },
+      { time: "3", events: "", },
+      { time: "4", events: "", },
+      { time: "5", events: "", },
+      ]
+} 
+
 // update the time block colors based on the time of day
 var keepTime = function() {
     var currentHour = moment().hour();
     currentHour = 13;
     $(".time-block").each (function(index) {
-      console.log(index);
       if (time[index] < currentHour) {
         $(this).removeClass(["present", "future"]).addClass("past");
       } else if (time[index] == currentHour) {
@@ -26,28 +52,23 @@ var keepTime = function() {
 // call function reflect current time in time block colors
 keepTime();
 
-// define all time blocks individually
-var scheduleArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-var scheduleTemplate = ["", "", "", "", "", "", "", "", ""]
+
 
 // function to save input to local storage
-$(".saveBtn").click (function() {
-  console.log("hello");
+$(".saveBtn").click (function(event) {
+event.preventDefault()
     $("textarea").each (function(index, value) {
-      console.log(value);
     var timeInput = $(value).attr("id")
-    var plannerInput = $(value).val();
-    localStorage.setItem(timeInput, plannerInput);
-     console.log(timeInput, plannerInput);
+    var plannerInput = $(value).val().trim();
+    console.log(scheduleArray);
+    for (let i = 0; i < scheduleArray.length; i++) {
+      if (timeInput === scheduleArray[i].time) {
+        scheduleArray[i].events = plannerInput;
+      }
+    }
+    console.log(scheduleArray);
+    localStorage.setItem("scheduleStorage", JSON.stringify(scheduleArray));
+     console.log(scheduleArray);
   })
 })
   
-// call to pull up local storage items
-var savedInput = localStorage.getItem("schedule");
-if (savedInput) {
-  scheduleTemplate = savedInput;
-  rerenderThePage();
-}
-
-scheduleItemsStored
-for loop
